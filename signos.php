d9ed49a1fe54741fb245242e41a70973080e45a1
Ademildo Alves de Medeiros Júnior <32140135+adjmedeiros@users.noreply.github.com>
2022-10-14 09:32:50 -0300

    signos.php

---
+++ signos.php
@@ -0,0 +1,77 @@
+<?xml version="1.0"?>
+    <signos>
+        <signo>
+            <dataInicio>21/01</dataInicio>
+            <dataFim>19/02</dataFim>
+            <signoNome>Aquário</signoNome>
+            <descricao>Características positivas dos aquarianos: idealista e inventivo, o aquariano costuma ser tranquilo e justo, ao mesmo tempo que gosta de transformar e não se acomodar nas situações. Características negativas dos aquarianos: por ser individualista e desapegado, muitos consideram o aquariano frio e sem coração (rs), além de distante, rebelde e imprevisível.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>19/02</dataInicio>
+            <dataFim>20/03</dataFim>
+            <signoNome>Peixes</signoNome>
+            <descricao>Características positivas dos piscianos: ouvintes natos, são confiáveis, intuitivos, prestativos e preocupados com os demais. São adaptáveis e simpáticos, os tornando bem sociáveis.Características negativas dos piscianos: a sensibilidade aflorada pode os tornar emocionais demais, tendendo para a depressão, os tornando muito temperamentais e rancorosos. São avoados, escapistas da realidade e por confiarem muito nos outros também costumam ser indecisos e inocentes.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>21/03</dataInicio>
+            <dataFim>20/04</dataFim>
+            <signoNome>Áries</signoNome>
+            <descricao>Características positivas dos arianos: se coloca em primeiro lugar, mas sem esquecer dos demais à sua volta! Motiva as pessoas e é prestativo, além de perseverante e apaixonado.
+            Características negativas dos arianos: sua agitação faz com que o ariano normalmente seja inquieto, impulsivo ou inconsequente. Por pensar muito nele próprio também é às vezes taxado de egoísta.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>21/04</dataInicio>
+            <dataFim>20/05</dataFim>
+            <signoNome>Touro</signoNome>
+            <descricao>Características positivas dos taurinos: cuidadoso com tudo e com todos, é paciente e produtivo. Desse modo, são pessoas em que se podem confiar e costumam ser muito fiéis. 
+            Características negativas dos taurinos: a teimosia é característica nata de um taurino. Visto que são muito persistentes e obstinado, querem cuidar de tudo e podem se mostrar centralizadores, inflexíveis e materialistas.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>21/05</dataInicio>
+            <dataFim>20/06</dataFim>
+            <signoNome>Gêmeos</signoNome>
+            <descricao>Características positivas dos geminianos: animados e comunicativos, eles tem a mente aberta e são muito espertos.
+            Características negativas dos geminianos: podem oscilar muito, tonando-se indecisos, inconstantes, ansiosos e ao mesmo tempo evasivos</descricao>
+        </signo>
+        <signo>
+            <dataInicio>21/06</dataInicio>
+            <dataFim>22/07</dataFim>
+            <signoNome>Câncer</signoNome>
+            <descricao>Características positivas dos cancerianos: são dedicados e gentis, sempre ouvindo ou cuidando de alguém por perceberam suas emoções e problemas.
+            Características negativas dos cancerianos: a carência e o mau humor podem surgir a qualquer momento nos cancerianos, que podem se tornar ciumentos ou se vitimizar.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>23/07</dataInicio>
+            <dataFim>22/08</dataFim>
+            <signoNome>Leão</signoNome>
+            <descricao>Características positivas dos leoninos: inteligentes e corajosos, são líderes natos. Por outro lado, são expressivos e determinados: quando colocam algo na mente, pode ter certeza que vão até o fim para conquistá-la.
+            Características negativas dos leoninos: têm a personalidade muito forte, podendo se tornar ambiciosos, mandões ou convencidos demais.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>23/08</dataInicio>
+            <dataFim>22/09</dataFim>
+            <signoNome>Virgem</signoNome>
+            <descricao>Características positivas dos virginianos: observação e eficiência são marcas do virginiano, que é bastante atencioso e sincero com os demais e está sempre resolvendo um problema: seja seu ou dos outros.
+            Características negativas dos virginianos: ser sistemático torna o virginiano cheio de manias, muito crítico e desconfiado. Também pode torná-los ansiosos e perfeccionistas demais.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>23/09</dataInicio>
+            <dataFim>22/10</dataFim>
+            <signoNome>Libra</signoNome>
+            <descricao>Características positivas dos librianos: extremamente diplomáticos, são sinceros e buscam ponderar sempre para chegar no melhor resultado. Para isso contam com sua simpatia, boa comunicação e tranquilidade.
+            Características negativas dos librianos: querer atingir o equilíbrio sempre torna o libriano indeciso, controlador e com muita mania de organização. Também têm dificuldade de dizer não em algumas situações.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>23/10</dataInicio>
+            <dataFim>21/11</dataFim>
+            <signoNome>Escorpião</signoNome>
+            <descricao>Características positivas dos escorpianos: de uma auto-confiança invejável (a não ser pelos leoninos…rs), têm muita intuição e um instinto protetor maravilhoso. São dedicados e apaixonados.    Características negativas dos escorpianos: sua boa memória, ciúmes e às vezes obsessão fazem com que o nunca esqueçam do que lhe fizeram, se tornando vingativos e rancorosos.</descricao>
+        </signo>
+        <signo>
+            <dataInicio>22/11</dataInicio>
+            <dataFim>21/12</dataFim>
+            <signoNome>Sagitário</signoNome>
+            <descricao>Características positivas dos sagitarianos: são talentosos, inteligentes e justos. São pessoas estimulantes, que gostam de se divertir e têm bastante espontaneidade e vitalidade. 
+            Características negativas dos sagitarianos: podem aparentar arrogância por saberem demais, rígidos com si mesmos e os outros, um pouco impacientes e sarcásticos.</descricao>
+        </signo>
+    </signos>
